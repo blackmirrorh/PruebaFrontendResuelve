21cb7bba2dd63e6d4c4c44916b12884d8b37263d
Luis Ramirez <luislibra90@hotmail.com>
2021-09-07 14:25:39 -0500

    Agregado componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './app/search/search.component';
+import { CardComponent } from './app/movies/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
